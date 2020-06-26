2277731a1ac7fa90f9c02f0bed6a2dd1b8b2ae7e
pradel <pradel.leo@gmail.com>
2020-06-26 16:19:29 +0200

    lint

--- src/pages/Accounts.tsx
+++ src/pages/Accounts.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Box, IconButton, Flex, Text } from "@chakra-ui/core";
-import { Plus } from "react-feather";
-import Tooltip from "@reach/tooltip";
+import { Box, Text } from "@chakra-ui/core";
 import { AccountItem } from "../components/AccountItem";
 import { HeaderInfos } from "../components/HeaderInfos";
 import { useAppConfig } from "../context/AppConfigContext";
@@ -12,21 +10,12 @@ export const Accounts = () => {
   return (
     <Box px={4} py={4} maxWidth="1024px" margin="0 auto">
       <HeaderInfos />
+
       <Text>{appConfig.mnemonic}</Text>
+
       {Array.from(Array(appConfig.numberDisplayed).keys()).map((_, index) => (
         <AccountItem key={index} derivationIndex={index} />
       ))}
-
-      {/* <Flex pt={4} justifyContent="center">
-        <Tooltip label="Generate a new account">
-          <IconButton
-            aria-label="Generate new"
-            icon={Plus}
-            onClick={handleGenerateNewKey}
-          />
-        </Tooltip>
-      </Flex>
-      */}
     </Box>
   );
 };
