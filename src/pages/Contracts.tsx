6fb339d1c036c646ae818452e9e81577e01e972c
pradel <pradel.leo@gmail.com>
2020-06-27 17:41:17 +0200

    prepare ui for deploy

--- src/pages/Contracts.tsx
+++ src/pages/Contracts.tsx
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Box, List, ListItem, Button } from "@chakra-ui/core";
+import {
+  Box,
+  List,
+  ListItem,
+  Button,
+  Flex,
+  Input,
+  FormLabel,
+  FormControl,
+  Grid,
+} from "@chakra-ui/core";
 import { FileText } from "react-feather";
 import { useAppConfig } from "../context/AppConfigContext";
 import { Header } from "../components/Header";
@@ -9,6 +19,7 @@ const { ipcRenderer } = window.require("electron");
 export const Contracts = () => {
   const { appConfig } = useAppConfig();
   const [files, setFiles] = useState<string[]>();
+  const [selectedFile, setSelectedFile] = useState<string>();
 
   useEffect(() => {
     const getContractFiles = async () => {
@@ -29,22 +40,42 @@ export const Contracts = () => {
       <Header />
 
       <Box px={4} py={4} maxWidth="1024px" margin="0 auto">
-        {files && (
-          <List spacing="3">
-            {files.map((file) => (
-              <ListItem key={file}>
-                <Button
-                  leftIcon={FileText}
-                  variant="ghost"
-                  size="sm"
-                  color="gray.700"
-                >
-                  {file}
-                </Button>
-              </ListItem>
-            ))}
+        <Flex>
+          <List spacing="3" mr="8">
+            {files &&
+              files.map((file) => (
+                <ListItem key={file}>
+                  <Button
+                    leftIcon={FileText}
+                    variant="ghost"
+                    size="sm"
+                    color="gray.700"
+                    isActive={selectedFile === file}
+                    onClick={() => setSelectedFile(file)}
+                  >
+                    {file}
+                  </Button>
+                </ListItem>
+              ))}
           </List>
-        )}
+          {selectedFile && (
+            <Box flex="1">
+              <Grid templateColumns="1fr 1fr" gap={6}>
+                <FormControl flex="1">
+                  <FormLabel htmlFor="name">Contract name</FormLabel>
+                  <Input id="name" />
+                </FormControl>
+                <FormControl flex="1">
+                  <FormLabel htmlFor="fee">Fee</FormLabel>
+                  <Input id="fee" />
+                </FormControl>
+              </Grid>
+              <Button mt="6" variantColor="teal">
+                Deploy
+              </Button>
+            </Box>
+          )}
+        </Flex>
       </Box>
       <Footer />
     </React.Fragment>
