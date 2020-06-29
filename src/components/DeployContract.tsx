bf0dbed2fa9c7a7a65919e471bc6b9ce456b85b5
pradel <pradel.leo@gmail.com>
2020-06-29 21:58:27 +0200

    fix: fix contract deployment

--- src/components/DeployContract.tsx
+++ src/components/DeployContract.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Grid,
@@ -8,6 +8,10 @@ import {
   Button,
   useToast,
   Select,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
 } from "@chakra-ui/core";
 import { useFormik, FormikErrors } from "formik";
 import {
@@ -34,6 +38,7 @@ interface DeployContractProps {
 export const DeployContract = ({ contractFileName }: DeployContractProps) => {
   const { appConfig } = useAppConfig();
   const toast = useToast();
+  const [success, setSuccess] = useState<string>();
   const formik = useFormik<DeployContractValues>({
     initialValues: {
       name: "",
@@ -53,6 +58,7 @@ export const DeployContract = ({ contractFileName }: DeployContractProps) => {
       return errors;
     },
     onSubmit: async (values, { setSubmitting }) => {
+      setSubmitting(true);
       const network = new StacksTestnet();
 
       let transaction: StacksTransaction;
@@ -86,8 +92,21 @@ export const DeployContract = ({ contractFileName }: DeployContractProps) => {
       }
 
       try {
-        const data = await broadcastTransaction(transaction, network);
-        console.log(data);
+        const txId = await broadcastTransaction(transaction, network);
+        console.log(txId);
+        if (typeof txId === "string") {
+          setSuccess(txId);
+        } else {
+          toast({
+            title: "Failed to broadcast transaction.",
+            description: `${txId.error} - ${txId.reason}`,
+            status: "error",
+            duration: 9000,
+            isClosable: true,
+          });
+          setSubmitting(false);
+          return;
+        }
       } catch (error) {
         console.error(error);
         toast({
@@ -100,65 +119,89 @@ export const DeployContract = ({ contractFileName }: DeployContractProps) => {
         setSubmitting(false);
         return;
       }
+
+      setSubmitting(false);
     },
   });
 
   return (
     <Box flex="1">
-      <form onSubmit={formik.handleSubmit}>
-        <Grid templateColumns="1fr 1fr" gap={6}>
-          <FormControl flex="1">
-            <FormLabel htmlFor="name">Contract name</FormLabel>
-            <Input
-              id="name"
-              name="name"
-              onChange={formik.handleChange}
-              value={formik.values.name}
-            />
-          </FormControl>
-          <FormControl flex="1">
-            <FormLabel htmlFor="fee">Fee</FormLabel>
-            <Input
-              id="fee"
-              name="fee"
-              onChange={formik.handleChange}
-              value={formik.values.fee}
-            />
-          </FormControl>
-          <FormControl flex="1">
-            <FormLabel htmlFor="accountIndex">Account</FormLabel>
-            <Select
-              id="accountIndex"
-              placeholder="Select account"
-              onChange={formik.handleChange}
-              value={formik.values.accountIndex}
-            >
-              {Array.from(Array(appConfig.numberDisplayed).keys()).map(
-                (_, index) => (
-                  <option key={index} value={index}>
-                    Account {index} -{" "}
-                    {
-                      getAccountFromDerivationPathIndex(
-                        appConfig.rootNode,
-                        index
-                      ).address
-                    }
-                  </option>
-                )
-              )}
-            </Select>
-          </FormControl>
-        </Grid>
-        <Button
-          mt="6"
-          variantColor="teal"
-          type="submit"
-          isLoading={formik.isSubmitting}
-          isDisabled={formik.isSubmitting}
+      {!success && (
+        <form onSubmit={formik.handleSubmit}>
+          <Grid templateColumns="1fr 1fr" gap={6}>
+            <FormControl flex="1">
+              <FormLabel htmlFor="name">Contract name</FormLabel>
+              <Input
+                id="name"
+                name="name"
+                onChange={formik.handleChange}
+                value={formik.values.name}
+              />
+            </FormControl>
+            <FormControl flex="1">
+              <FormLabel htmlFor="fee">Fee</FormLabel>
+              <Input
+                id="fee"
+                name="fee"
+                onChange={formik.handleChange}
+                value={formik.values.fee}
+              />
+            </FormControl>
+            <FormControl flex="1">
+              <FormLabel htmlFor="accountIndex">Account</FormLabel>
+              <Select
+                id="accountIndex"
+                placeholder="Select account"
+                onChange={formik.handleChange}
+                value={formik.values.accountIndex}
+              >
+                {Array.from(Array(appConfig.numberDisplayed).keys()).map(
+                  (_, index) => (
+                    <option key={index} value={index}>
+                      Account {index} -{" "}
+                      {
+                        getAccountFromDerivationPathIndex(
+                          appConfig.rootNode,
+                          index
+                        ).address
+                      }
+                    </option>
+                  )
+                )}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Button
+            mt="6"
+            variantColor="teal"
+            type="submit"
+            isLoading={formik.isSubmitting}
+            isDisabled={formik.isSubmitting}
+          >
+            Deploy
+          </Button>
+        </form>
+      )}
+
+      {success && (
+        <Alert
+          status="success"
+          variant="subtle"
+          flexDirection="column"
+          justifyContent="center"
+          textAlign="center"
+          height="200px"
         >
-          Deploy
-        </Button>
-      </form>
+          <AlertIcon size="40px" mr={0} />
+          <AlertTitle mt={4} mb={1} fontSize="lg">
+            Contract deployed.
+          </AlertTitle>
+          <AlertDescription maxWidth="sm">
+            The contract was successfully deployed, the transaction id is{" "}
+            {success}.
+          </AlertDescription>
+        </Alert>
+      )}
     </Box>
   );
 };
